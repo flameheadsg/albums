cb599140bf2e164d465f66deeb716013afdc5d5d
Sunny Gupta <flameheadsg@gmail.com>
2018-07-16 23:30:45 -0700

    initialized repo

---
+++ App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <Text>My first React-Native application!</Text>
+    );
+  }
+}
